d191ffe7a1775bd3de932a82319c8ce03ad05f20
reberti <jlreberti@ext.worten.pt>
2021-11-22 08:25:39 +0000

    monolith

---
+++ nest-ambassador/src/order/listeners/order.listener.ts
@@ -0,0 +1,31 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { Order } from '../order';
+import { OnEvent } from '@nestjs/event-emitter';
+import { RedisService } from '../../shared/redis.service';
+import { MailerService } from '@nestjs-modules/mailer';
+import { ClientKafka } from '@nestjs/microservices';
+
+@Injectable()
+export class OrderListener {
+  constructor(
+    private redisService: RedisService,
+    @Inject('KAFKA_SERVICE') private client: ClientKafka,
+  ) {}
+
+  @OnEvent('order.completed')
+  async handleOrderCompletedEvent(order: Order) {
+    const client = this.redisService.getClient();
+    client.zincrby('rankings', order.ambassador_revenue, order.user.name);
+
+
+    const data = {
+        ...order,
+        total: order.total,
+        ambassador_revenue: order.ambassador_revenue,
+    }
+
+
+    //produce a message
+    await this.client.emit('default', JSON.stringify(data));
+  }
+}
